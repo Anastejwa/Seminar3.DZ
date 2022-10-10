012407708dfb662ba1d3e50389c365d5e0308014
Anastejwa <martynovaa709@gmail.com>
2022-10-10 22:30:18 +0300

    Решение

---
+++ Задача19/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+Console.Clear();
+Console.WriteLine("Программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. ");
+
+//Объявление массива
+int[] arr = new int [5];
+int bigNumber;
+int del = 10000;
+int index = 0;
+
+
+Console.WriteLine("Введите пятизначное число: ");
+bigNumber = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = bigNumber / del % 10;
+        del /= 10;
+    }
+
+if ((arr[index] == arr[arr.Length-1]) && (arr[index+1] == arr[arr.Length-2]))
+    {Console.WriteLine($"Число является палиндромом "); }
+else
+    {Console.WriteLine($"Число не является палиндромом "); }
